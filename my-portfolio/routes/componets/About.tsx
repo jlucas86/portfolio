44a68dd57441df454e3e3890b335ae4f63d483c2
jlucas86 <joshua.lucas014@gmail.com>
2022-08-14 21:38:06 -0400

    updatd about section, added new description and changed styling

--- my-portfolio/routes/componets/About.tsx
+++ my-portfolio/routes/componets/About.tsx
@@ -12,15 +12,18 @@ import { tw } from "twind";
 
 export default function About() {
     return (
-        <div class={tw`bg-primary text-white pt-14 px-10 h-screen`}>
-            <h1 class={tw`text-3xl py-4`}>Joshua Lucas</h1>
-            <p class={tw`text-xl pb-4`}> 
-                I am a software deveoper with a B.S in computer science from UMBC. My goals as a software developer are to produce
-                high quality software to solve modernday problems from large to small. One such problem is the ability to find good 
-                quality food where ever you go. I am curently working on this problem by developing a webapp to allow friend, restuarants, 
-                and reveiwers/influencers to communicat, post reviews, and vote on the helpfullness of reveiws. 
-                
-            </p>
+        <div class={tw`bg-primary text-white pt-14 px(10 lg:64 xl:80 2xl:96) h-screen flex flex-col items-center justify-center`}>
+            <div class={tw``}>
+                <h1 class={tw`text-3xl py-4`}>Hello, my name is <span class={tw`text-textS`}>Joshua Lucas</span>.</h1>
+                <p class={tw`text-xl pb-4`}> 
+                    I am a software deveoper, currently focusing on full stack development. 
+                    I graduated from University of Maryland, Baltimore County (UMBC), with B.S. in computer science.
+                    Since graduating, I have worked on few freelace projects of varying types.
+                    As well as personal projects, with the goal of providing daily utiltiy to grow a large use base. 
+                    
+                </p>
+            </div>
+            
         </div>
       );
   }
