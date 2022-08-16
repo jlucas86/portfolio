78616240589d33f5626f81b8471903e90a014298
jlucas86 <joshua.lucas014@gmail.com>
2022-08-16 01:20:18 -0400

    rudementry animation started for about me

--- my-portfolio/routes/componets/About.tsx
+++ my-portfolio/routes/componets/About.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "twind";
+import { useState, useEffect, useRef} from "preact/hooks";
 
 // TODO - determin what information to display
 // TODO - determin background (mayeb a simple n-body simulation)
@@ -11,6 +12,24 @@ import { tw } from "twind";
  */
 
 export default function About() {
+
+    let postionY: [number, number, number, number, number, number, number, number, number, number] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+    const [posX, setPosX] = useState(10);
+    const [posY, setPosY] = useState(postionY);
+
+    useEffect(()=> {
+        setTimeout(() => { 
+            console.log(posX)
+            if(posX >= 100){
+                setPosX(10)
+            }else{
+                setPosX(posX+.2)
+                setPosY(posY)
+            }
+            
+         }, 5);
+    },[posX])
     return (
         <div class={tw`bg-primary text-white pt-14 px(10 lg:64 xl:80 2xl:96) h-screen flex flex-col items-center justify-center`}>
             <div class={tw``}>
@@ -24,7 +43,16 @@ export default function About() {
             </div>
             <svg viewBox="0 0 200 100" >
 
-                <circle cx="60" cy="50" r="5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX} cy={posY[0]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+10} cy={posY[1]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+20} cy={posY[2]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+30} cy={posY[3]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+40} cy={posY[4]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+50} cy={posY[5]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+60} cy={posY[6]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+70} cy={posY[7]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+80} cy={posY[8]} r="2.5" stroke="black" stroke-width="0" fill="white" />
+                <circle cx={posX+90} cy={posY[9]} r="2.5" stroke="black" stroke-width="0" fill="white" />
                 </svg>
             
         </div>
