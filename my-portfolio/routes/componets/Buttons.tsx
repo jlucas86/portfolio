69b22f1c223ab6dc6e5425e299a64f1e3d795d94
jlucas86 <joshua.lucas014@gmail.com>
2022-08-22 00:40:20 -0400

    button styling, broke c image for skills

--- my-portfolio/routes/componets/Buttons.tsx
+++ my-portfolio/routes/componets/Buttons.tsx
@@ -5,13 +5,13 @@ import { tw } from "twind";
 export default function Buttons() {
 
     return (
-        <div class={tw`fixed flow-root h-10 w-20 pt-4 bottom-80 ml-auto bg-red-500`}>
+        <div class={tw`fixed h-10 w-40 pt-4 bottom-80 right-0 `}>
           <div class={tw`flex flex-col content-center justify-center `}>
               <input 
                 type="image" 
                 src="/img/email.png" 
                 alt="email"
-                class={tw`w-10 h-10 p-8 mx-10 my-5 bg-button rounded-lg bg-cover`}
+                class={tw`w-16 h-16 p-1 mx-10 my-5 bg-button rounded-lg bg-cover`}
                 onclick="console.log(window.pageYOffset)"
               />
               <input 
@@ -19,13 +19,13 @@ export default function Buttons() {
                 src="/img/github.png"
                 alt="github" 
                 onclick="window.open('https://github.com/jlucas86')"
-                class={tw`w-10 h-10 p-8 mx-10 my-5 bg-button rounded-lg bg-cover`}
+                class={tw`w-16 h-16 p-1 mx-10 my-5 bg-button rounded-lg bg-cover`}
               />
               <input 
                 type="image" 
                 src="/img/linkedin.png" 
                 alt="linkedIn"
-                class={tw`w-10 h-10 p-8 mx-10 my-5 bg-button rounded-lg bg-cover`}
+                class={tw`w-16 h-16 p-1 mx-10 my-5 bg-button rounded-lg bg-cover`}
               />
               
           </div>
